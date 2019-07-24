74e51208c0929f90754281090b83a33d25a2d064
Jung Sim <tlawjd99gma@gmail.com>
2019-07-24 15:35:14 +0900

    First commit

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "+ : Add\t- : Subtract\t\n";
+	char op;
+	int a, b;
+	while (cin >> op)
+	{
+		switch (op)
+		{
+		case '+':
+			cout << "Put two numbers\n";
+			cin >> a >> b;
+			cout << a << '+' << b << '=' << a + b << '\n';
+			break;
+		case '-':
+			cout << "Put two numbers\n";
+			cin >> a >> b;
+			cout << a << '-' << b << '=' << a - b << '\n';
+			break;
+		default:
+			return 0;
+		}
+	}
+	return 0;
+}
